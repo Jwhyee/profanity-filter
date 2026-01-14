758678359f9ab0c695290e7f302e5bf03787e192
Jwhyee <kawnsdud@naver.com>
2026-01-14 12:42:20 +0900

    refactor : 배포를 위한 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,37 @@
 plugins {
     kotlin("jvm") version "2.2.20"
+    `maven-publish`
 }
 
-group = "com.project"
-version = "1.0-SNAPSHOT"
+group = "io.github.jwhyee.profanity"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.ahocorasick:ahocorasick:0.6.3")
+    api("org.ahocorasick:ahocorasick:0.6.3")
     testImplementation(kotlin("test"))
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(21)
 }
