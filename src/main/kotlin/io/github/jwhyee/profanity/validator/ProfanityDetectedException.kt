621aa061ae6241f85ca1f3c71380651e79a8bb11
Jwhyee <kawnsdud@naver.com>
2026-01-16 17:44:00 +0900

    Exception 데이터를 활용할 수 있도록 변수화

--- src/main/kotlin/io/github/jwhyee/profanity/validator/ProfanityDetectedException.kt
+++ src/main/kotlin/io/github/jwhyee/profanity/validator/ProfanityDetectedException.kt
@@ -1,5 +1,5 @@
 package io.github.jwhyee.profanity.validator
 
 class ProfanityDetectedException(
-    detected: List<String>
+    val detected: List<String>
 ) : RuntimeException("금칙어가 포함되어 있습니다: $detected")
